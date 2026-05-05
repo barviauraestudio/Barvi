5fbd704af23d4d1b0a78a588eb7c16d5b532033f
barviauraestudio <barviauraestudio@gmail.com>
2026-05-04 21:43:54 -0300

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -40,6 +40,7 @@ export default function CTA() {
               rel="noopener noreferrer"
             >
               Conheça Nossa Consultoria
+              Consultoria
               <ArrowIcon />
             </a>
           </div>
